3b252d06245128c05157898553f9c5f69a6c1382
alicerichardeau <alicerichardeau@outlook.com>
2022-05-06 15:40:25 +0200

    navbar

--- public/js/global-scripts.js
+++ public/js/global-scripts.js
@@ -6,15 +6,43 @@ let elementTop = document.querySelector('.nav-top .top');
 
 window.onscroll = function() {
 
+  if(document.documentElement.scrollTop > 80) {
 
-        if(document.documentElement.scrollTop > 80) {
-      
-          elementTop.style.display = 'none';
-    
-        }
-    
-        else {
-      
-            elementTop.style.display = 'flex';
-        }
+    elementTop.style.display = 'none';
+
+  }
+
+  else {
+
+      elementTop.style.display = 'flex';
+  }
+}
+
+// burger menu to cross on click
+// ##############################
+
+let link = document.getElementById('link');
+let burger = document.getElementById('burger');
+let menu = document.querySelector('.navbar .menu');
+let nav = document.querySelector('.navbar');
+
+link.addEventListener('click', function() {
+
+    // if menu is openned
+    if(burger.classList.contains('open')) {
+
+        menu.style.display = "none";
+        burger.classList.remove("open");
+        menu.classList.remove('load');
+        nav.classList.remove('load');
+
     }
+    // if menu is closed
+    else {
+        
+        burger.classList.add('open');
+        menu.classList.add('load');
+        nav.classList.add('load');
+        menu.style.display = "flex";
+    }
+})
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,11 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="app_default")
+     * @Route("/", name="default")
      */
     public function index(ArticleRepository $articleRepository): Response
     {
@@ -26,10 +27,11 @@ class DefaultController extends AbstractController
     /**
      * @Route("/article-blog/{slug}", name="detail_article", methods={"GET"})
      */
-    public function getOneArticle(Article $article): Response
+    public function getOneArticle(Article $article, ArticleRepository $articleRepository): Response
     {
         return $this->render('article/detail_article.html.twig', [
             'article' => $article,
+            'articles' => $articleRepository->findAll(),
         ]);
     }
 }
